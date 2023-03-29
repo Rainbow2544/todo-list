4697a254b9ac867e9c72c1881b98e6331288308c
Rainbow <kenesou@gmail.com>
2023-03-29 17:09:21 +1300

    create navbar and home page

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from 'styled-components';
+
+const Container = style.div`
+    width: 100%;
+`
+
+function NavBar() {
+  return (
+    <Container>
+        <p>where</p>
+    </Container>
+  )
+}
+
+export default NavBar;
